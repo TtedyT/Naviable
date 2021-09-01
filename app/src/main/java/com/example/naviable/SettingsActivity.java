22c0edebdd5a2406b1bff8da75f7b95ba5691200
Your Name <you@example.com>
2021-09-01 14:30:31 +0300

    made settings layout and activity

---
+++ app/src/main/java/com/example/naviable/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.example.naviable;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Spinner spinner = findViewById(R.id.campus_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.campuses_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+}
