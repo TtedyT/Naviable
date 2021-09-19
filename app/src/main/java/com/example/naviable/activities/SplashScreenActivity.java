ef2321132bb9effa964d5f68b4218acbd83c3980
jasonmg <jasonmg@cs.huji.ac.il>
2021-09-19 11:03:16 +0300

    moved splash screen

--- app/src/main/java/com/example/naviable/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/naviable/activities/SplashScreenActivity.java
@@ -1,10 +1,12 @@
-package com.example.naviable;
+package com.example.naviable.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import com.example.naviable.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
